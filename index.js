0121f7d3ca5fc80d52d35504ad5c224350e4f2c7
Jan Rexer <crypto@rexer.dev>
2021-11-20 12:37:17 +0100

    Reformatted code and added some comments, if someone might not understands the code. Also removed plesk default page from repo^^

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Add required libs
-const { Client, Intents } = require('discord.js');
+const {Client, Intents} = require('discord.js');
 const axios = require('axios');
 
 // Initialise Discord client
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
 // Load .env file
 require('dotenv').config();
@@ -22,9 +22,9 @@ let nftProjects = process.env.NFT_PROJECT_LIST.split(",");
 // ###
 // Add your Discord channel name to OpenSea collection-slug mapping here
 let channelMapping = {
-    '🦍boredapes':['boredapeyachtclub','mutant-ape-yacht-club','bored-ape-kennel-club'],
-    '👨cryptodads':['thecryptodads'],
-    '🦁lazy-lions':['lazy-lions']
+    '🦍boredapes': ['boredapeyachtclub', 'mutant-ape-yacht-club', 'bored-ape-kennel-club'],
+    '👨cryptodads': ['thecryptodads'],
+    '🦁lazy-lions': ['lazy-lions']
 }
 // ###
 // EDIT YOUR SETTINGS HERE - END
@@ -37,33 +37,38 @@ getFloor = (customProjects) => {
     let requests = [];
 
     // If user specifies a project, return the specific stats
-    if (customProjects !== undefined && customProjects.length > 0){
-        requests.push([customProjects,`${requestBase}/${customProjects}/stats`]);
-    }else{
+    if (customProjects !== undefined && customProjects.length > 0) {
+        requests.push([customProjects, `${requestBase}/${customProjects}/stats`]);
+    } else {
         // If user not specifies a project, return the stored projects stats
         nftProjects.forEach((projects) => {
-            requests.push([projects,`${requestBase}/${projects}/stats`]);
+            requests.push([projects, `${requestBase}/${projects}/stats`]);
         })
     }
-    
+
     return requests;
 }
 
-sendMessages = (request,message) => {
+sendMessages = (request, message) => {
     axios.get(request[1]).then(result => {
+        // Get floor price from result
         let floorPrice = result.data.stats.floor_price;
+
+        // Get human readable collection name from opensea
         let collectionRequest = `https://api.opensea.io/api/v1/collection/${request[0]}`;
         axios.get(collectionRequest).then(result => {
+            // Send floor price info to channel
             message.channel.send(`:small_red_triangle_down: **${result.data.collection.name}** Floor Price: ${floorPrice}ETH`);
         })
     }).catch(function (error) {
+        // Catch errors
         if (error.response) {
             let errorMessage = `:monkey_face: **404**: Collection not found. Please enter Opensea URL-slug. (example: boredapesyachtclub)`;
-            
-            if(error.response.status !== 404){
-                errorMessage = `:monkey_face: **${error.response.status}**`;
+            // If statuscode is different from 404, send the statuscode to the user.
+            if (error.response.status !== 404) {
+                errorMessage = `:monkey_face: **${error.response.status}** Please try again later.`;
             }
-
+            // Send error message
             message.channel.send(`${errorMessage}`);
         } else if (error.request) {
             message.channel.send(`:monkey_face: **Request Error**`);
@@ -77,8 +82,9 @@ callFloor = (collection, message) => {
     // Call floor function to send floor information as discord message
     let floorRequests = getFloor(collection);
 
+    // Send a message for each collections floor request
     floorRequests.forEach((request) => {
-        sendMessages(request,message);
+        sendMessages(request, message);
     })
 }
 
@@ -88,32 +94,34 @@ client.on('messageCreate', message => {
 
     // Check if message starts with desired prefix
     if (message.content.startsWith(prefix)) {
-        
+
         // Store message arguments in array
-        let args = message.content.slice(prefix.length+1).trim().split(' ');
+        let args = message.content.slice(prefix.length + 1).trim().split(' ');
 
         // If channel is whitelisted, allow custom project retrieving
-        if(botChannelWhitelist.includes(`${message.channel.name}`) && args[0] !== 'all'){
+        if (botChannelWhitelist.includes(`${message.channel.name}`) && args[0] !== 'all') {
             // If !floor <string> retrieve custom projects floor price
             callFloor(args[0], message);
-        }else if(args[0] !== 'all'){
+        } else if (args[0] !== 'all') {
             let channelCollection = channelMapping[`${message.channel.name}`];
             // If no matching mapping pair is found, load all defined collections from .env
-            if(channelCollection === undefined){
+            if (channelCollection === undefined) {
                 callFloor('', message);
-            }else{
+            } else {
                 // If !floor use the channelname mapping to retrieve floor price
                 channelCollection.forEach((collection) => {
                     callFloor(`${collection}`, message);
                 })
             }
-        }else if (args[0] === 'all' && botChannelWhitelist.includes(`${message.channel.name}`)){
+        } else if (args[0] === 'all' && botChannelWhitelist.includes(`${message.channel.name}`)) {
             // If !floor all use all given collections to retrieve floor price
             callFloor('', message);
-        }else{
+        } else {
+            // If !floor all is called in a not whitelisted channel, send error message to user.
             message.channel.send(`:monkey_face: **Channel restricted** Please move to your Discords bot channel.`);
         }
     }
 });
 
+// Bot login via Discord token from .env
 client.login(process.env.DISCORD_TOKEN);
